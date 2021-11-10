8e9ec9f23784c96c0e248aae1072659b3e4633a3
Francesco Didio <francescodedeoo@gmail.com>
2021-11-10 14:48:29 +0100

    hello world

--- src/Components/Tracker.js
+++ src/Components/Tracker.js
@@ -1,13 +1,14 @@
 import React, { useState, useEffect } from "react";
 import Navigations from "./AppBar";
 import axios from "axios";
-
+import { BottomNavigation, BottomNavigationAction } from "@mui/material";
 const url = {
   all: "https://disease.sh/v3/covid-19/all",
   countries: "https://disease.sh/v3/covid-19/countries",
 };
 export default function Tracker() {
   const [api, setApi] = useState([]);
+  const [value, setValue] = useState(0)
 
   const getCovidApi = async () => {
     const response = await axios.get(url.all);
@@ -22,6 +23,17 @@ export default function Tracker() {
     <div>
       <Navigations />
       <div>{JSON.stringify(api)}</div>
+      <BottomNavigation
+        showLabels
+        value={value}
+        onChange={(event, newValue) => {
+          setValue(newValue);
+        }}
+      >
+        <BottomNavigationAction label="Recents" />
+        <BottomNavigationAction label="Favorites" />
+        <BottomNavigationAction label="Nearby" />
+      </BottomNavigation>
     </div>
   );
 }
