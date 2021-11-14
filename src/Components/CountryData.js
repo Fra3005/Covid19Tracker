b8921a7700665b483e11130e376c0075326c8fa4
Francesco Didio <francescodedeoo@gmail.com>
2021-11-14 14:45:15 +0100

    start country

--- src/Components/CountryData.js
+++ src/Components/CountryData.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import { TextField, Button } from "@mui/material";
 
 const url = {
     countries: "https://disease.sh/v3/covid-19/countries/"
@@ -27,6 +28,12 @@ export default function CountryData () {
 
 
     return (
+        <>
+        <div style={{display:"flex", justifyContent:"center", marginTop:10}}>
+            <TextField variant="outlined" value={country} onChange={(e)=>{setCountry(e.target.value)}}></TextField>
+            <Button variant="contained" onClick={()=>{getApiCountry(country)}} style={{marginLeft:3}}>Cerca</Button>
+        </div>
         <div>{api?.country}</div>
+        </>
     )
 }
