5e42f61ab772b45cd6685474e695594aa56c8928
Francesco Didio <francescodedeoo@gmail.com>
2021-11-21 13:08:07 +0100

    add filtered array of nations

--- src/Components/CountryData.js
+++ src/Components/CountryData.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { TextField, Button } from "@mui/material";
+import { Pie } from "react-chartjs-2";
+
 
 const url = {
     countries: "https://disease.sh/v3/covid-19/countries/"
@@ -14,9 +16,24 @@ export default function CountryData () {
 
         const response = await axios.get(url.countries + `${country}`);
         setApi(response.data)
-
-
     }
+
+    const state1 = {
+        labels: [ "Popolazione", "Ricoverati"],
+        datasets: [
+          {
+            label: "Popolazione-Ricoverati",
+            backgroundColor: [
+              
+              'rgb(255, 205, 86)'
+            ] ,
+            borderColor: "rgba(0,0,0,1)",
+            borderWidth: 2,
+            data: [api?.populations, api?.recovered],
+          },
+        ],
+      };
+    
 
 
     useEffect(()=>{
@@ -24,7 +41,7 @@ export default function CountryData () {
         getApiCountry(country);
 
 
-    }, []);
+    }, [country]);
 
 
     return (
@@ -33,7 +50,23 @@ export default function CountryData () {
             <TextField variant="outlined" value={country} onChange={(e)=>{setCountry(e.target.value)}}></TextField>
             <Button variant="contained" onClick={()=>{getApiCountry(country)}} style={{marginLeft:3}}>Cerca</Button>
         </div>
-        <div>{api?.country}</div>
+        <div>
+        <Pie
+        data={state1}
+        options={{
+          plugins: {
+            title: {
+              display: true,
+              text: "Daily death in the world "
+            },
+            legend: {
+              display: true,
+              position: "bottom"
+           }
+          }
+        }}
+      />
+        </div>
         </>
     )
 }
--- src/Components/GeneralData.js
+++ src/Components/GeneralData.js
@@ -1,32 +1,62 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Bar } from "react-chartjs-2";
-import { Card, CardContent, Typography, CardActionArea, CardMedia } from "@mui/material";
-import virus from '../assets/virus.jpg';
-import ricoverati from '../assets/ricoverati.jpg';
-
+import {
+  Card,
+  CardContent,
+  Typography,
+  CardActionArea,
+  CardMedia,
+} from "@mui/material";
+import virus from "../assets/virus.jpg";
+import ricoverati from "../assets/ricoverati.jpg";
 
 const url = {
   all: "https://disease.sh/v3/covid-19/all",
+  countries: "https://disease.sh/v3/covid-19/countries",
 };
 
+const continenti = [
+  "Italy",
+  "USA",
+  "UK",
+  "Japan",
+  "Canada",
+  "China",
+  "Germany",
+  "France",
+  "India",
+];
+
 export default function GeneralData() {
+  const array = [];
   const [api, setApi] = useState([]);
   const [countryInfettati, setCountryInfettati] = useState(0);
   const [totalCases, setTotalCases] = useState(0);
   const [dailyCases, setDailyCases] = useState(0);
   const [deathCases, setDeathCases] = useState(0);
   const [dailyDeath, setDailyDeath] = useState(0);
+  const [continent, setContinent] = useState([]);
 
   const getCovidApi = async () => {
     const response = await axios.get(url.all);
     setCountryInfettati(response.data.affectedCountries);
-    setApi(response.data)
+    setApi(response.data);
     setTotalCases(response.data.cases);
     setDailyCases(response.data.todayCases);
     setDeathCases(response.data.deaths);
     setDailyDeath(response.data.todayDeaths);
-    
+  };
+
+  const getCovidCountries = async () => {
+    const response = await axios.get(url.countries);
+    setContinent(
+      response.data.map((item) => ({
+        nation: item.country,
+        value: item.todayCases,
+        total: item.cases
+      }))
+    );
   };
 
   const state = {
@@ -34,10 +64,7 @@ export default function GeneralData() {
     datasets: [
       {
         label: "Casi Giornalieri",
-        backgroundColor: [
-          'rgb(255, 99, 132)',
-          
-        ] ,
+        backgroundColor: ["rgb(255, 99, 132)"],
         borderColor: "rgba(0,0,0,1)",
         borderWidth: 2,
         data: [dailyCases],
@@ -45,92 +72,123 @@ export default function GeneralData() {
     ],
   };
 
+  
+
+  useEffect(() => {
+    getCovidApi();
+    getCovidCountries();
+  }, []);
+
+  useEffect(() => {
+    continent.forEach((element) => {
+      let country = element.nation;
+      for (let i = 0; i < continenti.length; i++) {
+        if (country === continenti[i]) {
+          array.push(element);
+        }
+      }
+    });
+    sortNationByCases(array);
+    console.log(array)
+  }, [continent]);
+
+
   const state1 = {
-    labels: [ "Morti Giornalieri"],
+    labels: [array[0]?.nation],
     datasets: [
       {
-        label: "Morti Giornalieri",
-        backgroundColor: [
-          
-          'rgb(255, 205, 86)'
-        ] ,
+        label: "Casi Totali",
+        backgroundColor: ["rgb(255, 205, 86)"],
         borderColor: "rgba(0,0,0,1)",
         borderWidth: 2,
-        data: [ dailyDeath],
+        data: [array[0]?.total],
       },
     ],
   };
 
-  useEffect(() => {
-    getCovidApi();
-  }, []);
+
+  const sortNationByCases = (a) =>{
+    a.sort(function(a, b) {
+      return (b.total) - (a.total);
+  });
+  }
 
   return (
     <>
-    <div style={{display:"flex", justifyContent:"space-evenly"}}>
-      <Card sx={{ width: 450, marginTop:8, marginBottom:5 }}>
-        <CardActionArea>
-        <CardMedia
-         component="img"
-         height="140"
-         image={virus}
-         alt="green iguana"/>
-        <CardContent>
-          <Typography><b>Casi Totali</b>: {totalCases} milioni</Typography>
-          <Typography><b>Morti Totali</b>: {deathCases} milioni</Typography>
-          <Typography><b>Nazioni Infettate</b>: {countryInfettati}</Typography>
-        </CardContent>
-        </CardActionArea>
-      </Card>
+      <div style={{ display: "flex", justifyContent: "space-evenly" }}>
+        <Card sx={{ width: 450, marginTop: 8, marginBottom: 5 }}>
+          <CardActionArea>
+            <CardMedia
+              component="img"
+              height="140"
+              image={virus}
+              alt="green iguana"
+            />
+            <CardContent>
+              <Typography>
+                <b>Casi Totali</b>: {totalCases} milioni
+              </Typography>
+              <Typography>
+                <b>Morti Totali</b>: {deathCases} milioni
+              </Typography>
+              <Typography>
+                <b>Nazioni Infettate</b>: {countryInfettati}
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
 
-      <Card sx={{ width: 450, marginTop:8, marginBottom:5 }}>
-        <CardActionArea>
-        <CardMedia
-         component="img"
-         height="140"
-         image={ricoverati}
-         alt="green iguana"/>
-        <CardContent>
-          <Typography><b>Ricoveri Totali</b>: {api.recovered} milioni</Typography>
-          <Typography><b>Ricoveri Giornalieri</b>: {api.todayRecovered} mila</Typography>
-        </CardContent>
-        </CardActionArea>
-      </Card>
-
-    </div>
-    <div style={{display:"flex", maxWidth:750}}>
-      <Bar
-        data={state}
-        options={{
-          plugins: {
-            title: {
-              display: true,
-              text: "Daily cases in the world"
+        <Card sx={{ width: 450, marginTop: 8, marginBottom: 5 }}>
+          <CardActionArea>
+            <CardMedia
+              component="img"
+              height="140"
+              image={ricoverati}
+              alt="green iguana"
+            />
+            <CardContent>
+              <Typography>
+                <b>Ricoveri Totali</b>: {api.recovered} milioni
+              </Typography>
+              <Typography>
+                <b>Ricoveri Giornalieri</b>: {api.todayRecovered} mila
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+      </div>
+      <div style={{ display: "flex", maxWidth: 750 }}>
+        {/* <Bar
+          data={state}
+          options={{
+            plugins: {
+              title: {
+                display: true,
+                text: "Daily cases in the world",
+              },
+              legend: {
+                display: true,
+                position: "bottom",
+              },
             },
-            legend: {
-              display: true,
-              position: "bottom"
-           }
-          }
-        }}
-      />
-      <Bar
-        data={state1}
-        options={{
-          plugins: {
-            title: {
-              display: true,
-              text: "Daily death in the world "
+          }}
+        /> */}
+        <Bar
+          data={state1}
+          options={{
+            plugins: {
+              title: {
+                display: true,
+                text: "Daily death in the world ",
+              },
+              legend: {
+                display: true,
+                position: "bottom",
+              },
             },
-            legend: {
-              display: true,
-              position: "bottom"
-           }
-          }
-        }}
-      />
-    
-    </div>
+          }}
+        />
+      </div>
     </>
   );
 }
