6810aaf01ebbda54a5e04b527c42e01d2bd5bccf
Francesco Didio <37405281+Fra3005@users.noreply.github.com>
2021-12-02 20:42:56 +0100

    add flag to country card

--- src/App.js
+++ src/App.js
@@ -3,27 +3,23 @@ import {
   Route,
   Link,
   Routes,
-  Navigate
+  Navigate,
 } from "react-router-dom";
 
 import GeneralData from "./Components/GeneralData";
-import CountryData from './Components/CountryData';
+import CountryData from "./Components/CountryData";
 
 import Tracker from "./Components/Tracker";
 function App() {
   return (
     <Router>
-    <Tracker />
-    <Routes>
-      <Route path="/General" element={ <GeneralData />}> 
-      </Route>  
-      <Route path="/country" element= {<CountryData/>}>
-        </Route>
-        <Route path="/" element= {<Navigate to={'/General'}/>}>
-        </Route>   
-    </Routes>
-  </Router>
-  
+      <Tracker />
+      <Routes>
+        <Route path="/General" element={<GeneralData />}></Route>
+        <Route path="/country" element={<CountryData />}></Route>
+        <Route path="/" element={<Navigate to={"/General"} />}></Route>
+      </Routes>
+    </Router>
   );
 }
 
--- src/Components/CountryData.js
+++ src/Components/CountryData.js
@@ -12,99 +12,105 @@ import {
 } from "@mui/material";
 
 const url = {
-    countries: "https://disease.sh/v3/covid-19/countries/"
-}
-export default function CountryData () {
-    
-    const [api, setApi] = useState(null);
-    const [country, setCountry] = useState("Italy");
+  countries: "https://disease.sh/v3/covid-19/countries/",
+};
+export default function CountryData() {
+  const [api, setApi] = useState(null);
+  const [country, setCountry] = useState("Italy");
 
-    const getApiCountry = async () =>{
+  const getApiCountry = async () => {
+    const response = await axios.get(url.countries + `${country}`);
+    setApi(response.data);
+  };
 
-        const response = await axios.get(url.countries + `${country}`);
-        setApi(response.data)
-    }
+  const state1 = {
+    labels: ["Popolazione", "Ricoverati"],
+    datasets: [
+      {
+        label: "Popolazione-Ricoverati",
+        backgroundColor: ["rgb(255, 205, 86)"],
+        borderColor: "rgba(0,0,0,1)",
+        borderWidth: 2,
+        data: [api?.populations, api?.recovered],
+      },
+    ],
+  };
 
-    const state1 = {
-        labels: [ "Popolazione", "Ricoverati"],
-        datasets: [
-          {
-            label: "Popolazione-Ricoverati",
-            backgroundColor: [
-              
-              'rgb(255, 205, 86)'
-            ] ,
-            borderColor: "rgba(0,0,0,1)",
-            borderWidth: 2,
-            data: [api?.populations, api?.recovered],
-          },
-        ],
-      };
-    
+  useEffect(() => {
+    getApiCountry(country);
+  }, [country]);
 
-
-    useEffect(()=>{
-
-        getApiCountry(country);
-
-
-    }, [country]);
-
-
-    return (
-        <>
-        <div style={{display:"flex", justifyContent:"center", marginTop:10}}>
-            <TextField variant="outlined" value={country} onChange={(e)=>{setCountry(e.target.value)}}></TextField>
-            <Button variant="contained" onClick={()=>{getApiCountry(country)}} style={{marginLeft:3}}>Cerca</Button>
-            </div>
-          <div>
-          <Card>
+  return (
+    <>
+      <Grid container spacing={2}>
+        <Grid item xs={2}></Grid>
+        <Grid item xs={7.5}>
+          <div
+            style={{ display: "flex", justifyContent: "center", marginTop: 4 }}
+          >
+            <TextField
+              variant="outlined"
+              value={country}
+              onChange={(e) => {
+                setCountry(e.target.value);
+              }}
+            ></TextField>
+            <Button
+              variant="contained"
+              onClick={() => {
+                getApiCountry(country);
+              }}
+              style={{ marginLeft: 3 }}
+            >
+              Cerca
+            </Button>
+          </div>
+        </Grid>
+        <Grid item xs={2.5}>
+          <div style={{ margin: 15 }}>
+            <Card>
               <CardActionArea>
                 <CardMedia
                   component="img"
-                  height="140"
-                  
-                  alt="green iguana"
+                  height="100"
+                  alt=""
+                  src={`https://flagcdn.com/16x12/${api?.countryInfo?.iso2?.toLowerCase()}.png`}
+                  srcset={[
+                    `https://flagcdn.com/32x24/${api?.countryInfo?.iso2?.toLowerCase()}.png 2x`,
+                    `https://flagcdn.com/48x36/${api?.countryInfo?.iso2?.toLowerCase()}.png 3x`,
+                  ]}
                 />
                 <CardContent>
                   <Typography>
-                    <b>Casi Totali</b>:  milioni
+                    <b>Popolazione</b>:{api?.population}
                   </Typography>
                   <Typography>
-                    <b>Morti Totali</b>:  milioni
-                  </Typography>
-                  <Typography>
-                    <b>Nazioni Infettate</b>: 
+                    <b>Casi oggi</b>: {api?.todayCases}
                   </Typography>
                 </CardContent>
               </CardActionArea>
             </Card>
           </div>
-        
-        <div>
+        </Grid>
+      </Grid>
+      <div></div>
+
+      <div>
         <Grid xs={6} sx={{ marginLeft: 2, marginTop: 8 }}>
-            <Card>
-              <CardActionArea>
-                <CardMedia
-                  component="img"
-                  height="140"
-                  
-                  alt="green iguana"
-                />
-                <CardContent>
-                  <Typography>
-                    <b>Casi Totali</b>:  milioni
-                  </Typography>
-                  <Typography>
-                    <b>Morti Totali</b>:  milioni
-                  </Typography>
-                  <Typography>
-                    <b>Nazioni Infettate</b>: 
-                  </Typography>
-                </CardContent>
-              </CardActionArea>
-            </Card>
-          </Grid>
+          <Card>
+            <CardActionArea>
+              <CardMedia component="img" height="140" alt="" />
+              <CardContent>
+                <Typography>
+                  <b>Popolazione</b>:{api?.population}
+                </Typography>
+                <Typography>
+                  <b>Casi oggi</b>: {api?.todayCases}
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+          </Card>
+        </Grid>
         {/* <Pie
         data={state1}
         options={{
@@ -120,7 +126,7 @@ export default function CountryData () {
           }
         }}
       /> */}
-        </div>
-        </>
-    )
+      </div>
+    </>
+  );
 }
