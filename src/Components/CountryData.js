a01dde4279028147556e52db10209197e37ce9f0
Fra3005 <francescodedeoo@gmail.com>
2021-12-06 22:27:18 +0100

    add pie chart

--- src/Components/CountryData.js
+++ src/Components/CountryData.js
@@ -6,9 +6,9 @@ import {
   Card,
   CardContent,
   Typography,
-  CardActionArea,
   CardMedia,
   Grid,
+  Box,
 } from "@mui/material";
 
 const url = {
@@ -24,14 +24,14 @@ export default function CountryData() {
   };
 
   const state1 = {
-    labels: ["Popolazione", "Ricoverati"],
+    labels: ["Popolazione", "Casi"],
     datasets: [
       {
         label: "Popolazione-Ricoverati",
-        backgroundColor: ["rgb(255, 205, 86)"],
+        backgroundColor: ["rgb(255, 205, 86)", "rgb(255, 99, 132)"],
         borderColor: "rgba(0,0,0,1)",
         borderWidth: 2,
-        data: [api?.populations, api?.recovered],
+        data: [api?.population, api?.cases],
       },
     ],
   };
@@ -39,6 +39,19 @@ export default function CountryData() {
   useEffect(() => {
     getApiCountry(country);
   }, [country]);
+
+  const state2 = {
+    labels: ["Casi", "Morti"],
+    datasets: [
+      {
+        label: "Casi Giornalieri",
+        backgroundColor: ["rgb(255, 99, 132)", "rgba(54, 162, 235, 0.2)"],
+        borderColor: "rgba(0,0,0,1)",
+        borderWidth: 2,
+        data: [api?.cases, api?.deaths],
+      },
+    ],
+  };
 
   return (
     <>
@@ -66,67 +79,86 @@ export default function CountryData() {
             </Button>
           </div>
         </Grid>
-        <Grid item xs={2.5}>
-          <div style={{ margin: 15 }}>
-            <Card>
-              <CardActionArea>
-                <CardMedia
-                  component="img"
-                  height="100"
-                  alt=""
-                  src={`https://flagcdn.com/16x12/${api?.countryInfo?.iso2?.toLowerCase()}.png`}
-                  srcset={[
-                    `https://flagcdn.com/32x24/${api?.countryInfo?.iso2?.toLowerCase()}.png 2x`,
-                    `https://flagcdn.com/48x36/${api?.countryInfo?.iso2?.toLowerCase()}.png 3x`,
-                  ]}
-                />
-                <CardContent>
+        <Grid item xs={2.5}></Grid>
+      </Grid>
+
+      <div>
+        <Grid container spacing={2}>
+          <Grid item xs={2}></Grid>
+          <Grid item xs={8} style={{ marginTop: 8 }}>
+            <Card sx={{ display: "flex", marginLeft: 10 }}>
+              <Box sx={{ display: "flex", flexDirection: "column" }}>
+                <CardContent sx={{ flex: "1 0 auto" }}>
+                  <Typography component="div" variant="h4">
+                    {country} Data
+                  </Typography>
                   <Typography>
                     <b>Popolazione</b>:{api?.population}
                   </Typography>
                   <Typography>
                     <b>Casi oggi</b>: {api?.todayCases}
                   </Typography>
+                  <Typography>
+                    <b>Ricoverati oggi</b>:{api?.todayRecovered}
+                  </Typography>
+                  <Typography>
+                    <b>Morti oggi</b>: {api?.todayDeaths}
+                  </Typography>
                 </CardContent>
-              </CardActionArea>
+                <Box
+                  sx={{ display: "flex", alignItems: "center", pl: 1, pb: 1 }}
+                ></Box>
+              </Box>
+              <CardMedia
+                component="img"
+                sx={{ width: 300, height: 150 }}
+                src={`https://flagcdn.com/16x12/${api?.countryInfo?.iso2?.toLowerCase()}.png`}
+                srcset={[
+                  `https://flagcdn.com/32x24/${api?.countryInfo?.iso2?.toLowerCase()}.png 2x`,
+                  `https://flagcdn.com/48x36/${api?.countryInfo?.iso2?.toLowerCase()}.png 3x`,
+                ]}
+              />
             </Card>
-          </div>
+          </Grid>
+        </Grid>
+      </div>
+
+      <Grid container spacing={2}>
+        <Grid item xs={6}>
+          <Pie
+            data={state1}
+            options={{
+              plugins: {
+                title: {
+                  display: true,
+                  text: "Rapporto Popolazione/Casi",
+                },
+                legend: {
+                  display: true,
+                  position: "bottom",
+                },
+              },
+            }}
+          />
+        </Grid>
+        <Grid item xs={6}>
+          <Pie
+            data={state2}
+            options={{
+              plugins: {
+                title: {
+                  display: true,
+                  text: "Rapporto Casi/Morti",
+                },
+                legend: {
+                  display: true,
+                  position: "bottom",
+                },
+              },
+            }}
+          />
         </Grid>
       </Grid>
-      <div></div>
-
-      <div>
-        <Grid xs={6} sx={{ marginLeft: 2, marginTop: 8 }}>
-          <Card>
-            <CardActionArea>
-              <CardMedia component="img" height="140" alt="" />
-              <CardContent>
-                <Typography>
-                  <b>Popolazione</b>:{api?.population}
-                </Typography>
-                <Typography>
-                  <b>Casi oggi</b>: {api?.todayCases}
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-          </Card>
-        </Grid>
-        {/* <Pie
-        data={state1}
-        options={{
-          plugins: {
-            title: {
-              display: true,
-              text: "Daily death in the world "
-            },
-            legend: {
-              display: true,
-              position: "bottom"
-           }
-          }
-        }}
-      /> */}
-      </div>
     </>
   );
 }
